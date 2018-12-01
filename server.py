3e4ca973b6c6b92b337cf1b83f947d71035f6dc9
Kelvin Jayanoris <kjayanoris@gmail.com>
2018-12-01 10:57:47 +0300

    Modify server to use connexion

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 """
 Basic web server
 """
-from flask import (
-    Flask,
-    render_template
-)
+import connexion
+from flask import render_template
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = connexion.App(__name__, specification_dir='swagger/')
+app.add_api('swagger.yml')
 
 
 # Create a URL route in our application for "/"
@@ -24,4 +23,4 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
